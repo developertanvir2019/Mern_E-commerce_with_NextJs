0cfbeeb9df8b2ac290b8676fcdbdab9ed0baf9f8
tanvir <tanvir101tk@gmail.com>
2023-03-30 17:53:43 +0600

    tailwind add success

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
+import Layout from '@/Component/Layout/Layout'
 import '@/styles/globals.css'
+import 'tailwindcss/tailwind.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Layout> <Component {...pageProps} /></Layout>
+  </>
 }
---
+++ pages/about/index.js
@@ -0,0 +1,10 @@
+const index = () => {
+    return (
+        <div>
+
+            <h2 className="text-8xl text-primary">about page</h2>
+        </div>
+    );
+};
+
+export default index;
